fb0f69f5ba9689442474a319b8ed4154ff6dcace
Frantisek Sumsal <frantisek@sumsal.cz>
2021-12-05 10:20:22 +0100

    More tests, more docs

--- cpp/UninitializedVariableWithCleanup/test.c
+++ cpp/UninitializedVariableWithCleanup/test.c
@@ -1,3 +1,4 @@
+#define NULL (void*)0
 #define _cleanup_foo_ __attribute__((__cleanup__(foo)))
 #define _cleanup_(x) __attribute__((__cleanup__(x)))
 
@@ -18,32 +19,35 @@ static inline void erase_char(char *p) {
 
 int main(void) {
     /* BAD: has a cleanup attribute, is a pointer, missing initialization */
-    __attribute__((__cleanup__(foo))) char *full_attribute;
+    __attribute__((__cleanup__(foo))) char *b_full_attribute;
     /* BAD: has a cleanup attribute (macrofied), is a pointer, missing initialization */
-    _cleanup_foo_ char *macro_attribute_1;
+    _cleanup_foo_ char *b_macro_attribute_1;
     /* BAD: has a cleanup attribute (macrofied), is a pointer, missing initialization */
-    _cleanup_free_ char *macro_attribute_and_fun_call;
+    _cleanup_free_ char *b_macro_attribute_and_fun_call;
     /* GOOD: doesn't have a cleanup attribute */
-    char *simple_pointer;
+    char *g_simple_pointer;
     /* GOOD: doesn't have a cleanup attribute */
-    char **double_pointer;
+    char **g_double_pointer;
     /* GOOD: has a cleanup attribute, is not a pointer */
-    _cleanup_(erase_char) char not_a_pointer;
+    _cleanup_(erase_char) char g_not_a_pointer;
+    /* GOOD: has a cleanup attribute, is a pointer, is initialized */
+    _cleanup_free_ char *g_macro_attribute_initialized = NULL;
     int r;
 
     /* We don't care if the variable is passed to a function, since the function
      * might not initialize it */
-    r = fun(&macro_attribute_and_fun_call);
+    r = fun(&b_macro_attribute_and_fun_call);
     if (r < 0)
         return 1;
 
-    puts(full_attribute);
-    puts(macro_attribute_1);
-    puts(macro_attribute_and_fun_call);
+    puts(b_full_attribute);
+    puts(b_macro_attribute_1);
+    puts(b_macro_attribute_and_fun_call);
 
-    puts(simple_pointer);
-    puts(*double_pointer);
-    printf("%c\n", not_a_pointer);
+    puts(g_simple_pointer);
+    puts(*g_double_pointer);
+    printf("%c\n", g_not_a_pointer);
+    puts(g_macro_attribute_initialized);
 
     return 0;
 }
